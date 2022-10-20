f6711f9af93ab325c562ab42d4ac28bf01bb52fc
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-10-20 15:44:35 +0900

    useRouter
    next 기본 탑재 useRouter 사용

--- nextjs-practice/components/NavBar.tsx
+++ nextjs-practice/components/NavBar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 //a태그 대신 nextjs에서는 link를 이용합시다.
 //새로고침이 안돼서 훨씬 빠르다 👍 !
+//link에는 href만을 넣어주고, 기타 css등 속성은 a쪽에 넣어주어야 함.
+
 export default function NavBar(){
+    const route=useRouter(); //next의 훅이다. next에 기본탑재되어있음
     return(
         <nav>
            <Link href="/"><a>Home</a></Link> 
@@ -9,3 +13,5 @@ export default function NavBar(){
         </nav>
     )
 }
+
+//🙂 라이브러리 버전을맞추느라 고생했던 지난날들은 무엇이었나....
