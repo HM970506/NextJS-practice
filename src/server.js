fd84d4879aa7cd55c9654e053a11502e71ab2759
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-10-25 12:18:30 +0900

    1챕터 끝
    주석을 이용한 일부 설명 추가

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
+//서버에서 구동되는 부분
+
 import express from "express"
 
 const app=express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+//유저는 public 폴더에 있는 내용은 볼 수 있다! 다른 부분은 못 본다
+
 app.get("/", (req, res) => res.render("home"));
+//라우터 부분
 
 console.log("Test");
 
