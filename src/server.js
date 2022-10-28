d289066efde6a25b8bb9364baf4e2aec108767d8
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-10-28 11:58:49 +0900

    Websocket대신 Socket.io를 사용해 봅시다
    Socket.io의 장점
    1. 끊김 없는 양방향 통신 제공 (카지노 웹사이트들에서 사용한다고 한다~)
    2. 웹소켓 크로스 브라우징 이슈 해결

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 //서버에서 구동되는 부분
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express"
+
 
 const app=express();
 app.set("view engine", "pug");
@@ -16,36 +17,19 @@ app.get("/*", (_, res) => res.redirect("/"));
 //request 를 _로 표시할 수 있어요
 
 console.log("서버를 시작합니당");
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-//여기선 http서버를 돌린다
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-//여기선 http서버 위에 웹소켓 서버를 돌린다
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
 
 //이렇게 하면 두 서버를 같이 돌릴 수 있어요
 //하지만 ws서버 하나만 만들어도 괜찮긴 해요
 
-const sockets=[];
-
-function handleConnection(socket) {
-  sockets.push(socket);
-    console.log("서버: 브라우저 연결");
-    socket.send("서버가 보낸 메시지입니다");
-    socket.on("close", ()=>{console.log("브라우저 닫힘")});
-
-    //웹소켓 버전이 8이 되면서, m으로만 보내면 blob 형태로 보내진다.
-    //toString으로 변환하여 보내주자
-    socket.on("message", (m)=>{sockets.forEach((s)=>s.send(m.toString()))});
-  }
-wss.on("connection", handleConnection);
-//event 처리처럼 서버.on으로 "이벤트", 실행함수를 적용할 수 있어요
-
-
-
+wsServer.on("connection", (socket) => {
+  console.log(socket);
+});
   
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 //서버 실행
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
 
 
